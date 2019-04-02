768b8e9a37ac7d658aadbb3830ba75912e444d56
josephttran <42635012+josephttran@users.noreply.github.com>
2019-04-02 02:51:26 -0700

    Add GraphQL schema definition

--- server/src/graphql/types.ts
+++ server/src/graphql/types.ts
@@ -3,5 +3,31 @@ import { gql } from 'apollo-server-koa';
 export const typeDefs = gql`
   type Query {
     hello: String
+    user(email: String!): User
+  }
+
+  type User {
+    id: ID
+    firstName: String
+    lastName: String
+    email: String
+    password: String
+  }
+
+  type Mutation {
+    registerUser(input: CreateUserInput!): AuthUser!
+  }
+  
+  type AuthUser {
+    id: ID!
+    email: String!
+    token: String!
+  }
+
+  input CreateUserInput {
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
   }
 `;
