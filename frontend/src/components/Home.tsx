96d7eb1bcd8c350a40aa443bb0a824330660ec81
josephttran <42635012+josephttran@users.noreply.github.com>
2019-04-14 03:28:47 -0700

    Add and handle logout button

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,4 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Home(){ return(<h2>Home</h2>)};
+import Auth from '../utils/auth';
+import { Redirect } from 'react-router-dom';
+
+const Home: React.FC = (props) => { 
+  const [isLoggedin, setIsLoggedIn] = useState(true);
+
+  const logout = () => {
+    console.log(Auth.isAuthenticated);  
+    Auth.logout();
+    setIsLoggedIn(false);
+
+    return <Redirect to='/login' />  
+  }
+
+  if (!Auth.isAuthenticated) {
+    return <Redirect to='/login' />        
+  } 
+
+  return (
+    <div>
+      <h2>Home</h2>
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
+};
+
 export default Home;
