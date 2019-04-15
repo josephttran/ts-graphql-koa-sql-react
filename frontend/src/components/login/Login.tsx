9e40896ffbe0d28c3ddcdd6645a381743e1fcd41
josephttran <42635012+josephttran@users.noreply.github.com>
2019-04-14 21:39:08 -0700

    Use mutation with login form, handle button click

--- frontend/src/components/login/Login.tsx
+++ frontend/src/components/login/Login.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Formik, Field, Form, FormikActions, ErrorMessage } from 'formik';
 
-import { LoginValues } from '../../interfaces/formInterface';
-import { validateLoginForm } from '../../utils/validation';
 import './Login.css';
 import { HelloQuery } from '../../graphql/query';
+import { LoginUserForm } from './LoginUserForm';
 
 const Login = () => {
   return(
@@ -13,45 +11,7 @@ const Login = () => {
       <div className='Login-header'>
         <h1>Login</h1>
       </div>
-      <div>
-        <Formik
-          initialValues={{
-            email: '',
-            password: ''
-          }}
-          onSubmit={(values: LoginValues, { setSubmitting }: FormikActions<LoginValues>) => {
-            setTimeout(() => {
-              console.log(JSON.stringify(values));
-              setSubmitting(false);
-            }, 500);
-          }}
-          validate={(values) => {
-            return validateLoginForm(values);
-          }}
-          render={() => (
-            <Form className='Login-form'>
-              <div className='Form-group'>
-                <label htmlFor='email'>
-                  <div className='Form-label'>Email</div>
-                </label>
-                <Field className='Form-input' id='email' name='email' placeholder='fl@example.com' type='email' />
-                <ErrorMessage className='Error' name='email' component='div' />              
-              </div>
-              <div className='Form-group'>
-                <label htmlFor='password'>
-                  <div className='Form-label'>Password</div>
-                </label> 
-                <Field className='Form-input' id='password' name='password' placeholder='password' type='password' />
-              </div>
-              <div className='Form-group'>
-                <button type='submit' style={{ display: 'block' }}>Login</button>
-                <p>Don't have an account?</p>
-                <a>Create Account</a>
-              </div>
-            </Form>
-          )}
-        />
-      </div>
+      <LoginUserForm />
     </div>
   )
 };
---
+++ frontend/src/components/login/LoginUserForm.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Redirect } from 'react-router';
+import { Mutation } from 'react-apollo';
+import { Formik, Field, Form, FormikActions, ErrorMessage } from 'formik';
+
+import { loginUserMutation } from '../../graphql/gqlMutation';
+import { ILoginValues } from '../../interfaces/formInterface';
+import { validateLoginForm } from '../../utils/validation';
+import Auth from '../../utils/auth';
+
+export const LoginUserForm = () => {
+  return (
+    <Mutation mutation={loginUserMutation()}>
+      {
+        (loginUser, result) => {
+          if(result.data) {
+            const token = result.data.loginUser.token;
+
+            if(token) {
+              localStorage.setItem('token', token);
+              localStorage.setItem('user', result.data.loginUser)
+              Auth.authenthicate();
+              
+              return <Redirect to='/' />
+            } 
+          }
+
+          return (
+            <Formik
+              initialValues={{ email: '', password: '' }}
+              validateOnBlur={false}
+              validateOnChange={false}
+              validate={(values) => {
+                const err = validateLoginForm(values);
+
+                if (err.email || err.password) {
+                  return err;
+                }
+              }}
+              onSubmit={async (values: ILoginValues, { setStatus, setSubmitting,  }: FormikActions<ILoginValues>) => {
+                try {
+                  await loginUser({variables: { email: values.email, password: values.password }});
+                  setSubmitting(false);
+                } catch(err) {
+                  setStatus({ status: 'error', message: 'Invalid user or password'});
+                }
+              }}
+              render={(props) => (
+                <Form className='Login-form'>
+                  <div className='Form-group'>
+                    <label htmlFor='email'>
+                      <div className='Form-label'>Email</div>
+                    </label>
+                    <Field className='Form-input' id='email' name='email' placeholder='fl@example.com' type='email' />
+                    <ErrorMessage className='Error' name='email' component='div' />              
+                  </div>
+                  <div className='Form-group'>
+                    <label htmlFor='password'>
+                      <div className='Form-label'>Password</div>
+                    </label> 
+                    <Field className='Form-input' id='password' name='password' placeholder='password' type='password' />
+                    <ErrorMessage className='Error' name='password' component='div' />                            
+                  </div>
+                  <div className='Form-group'>
+                    <button type='submit'>Login</button>
+                    <div className='Error'>{props.status ? props.status.message : ''}</div>
+                    <p>Don't have an account?</p>
+                    <a href='/register'>Create Account</a>
+                  </div>
+                </Form>
+              )}
+            />
+          )
+        }
+      }
+    </Mutation>
+  );
+};
